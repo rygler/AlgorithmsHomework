cc01daedd9ef2f04af548570e468e64496400b5c
Raphael Gal <raphaelgal@gmail.com>
2016-11-14 17:33:19 -0500

    changed Permutations

---
+++ src/KadaneMaximalSubArray.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Raphael on 11/14/2016.
+ */
+public class KadaneMaximalSubArray {
+}
--- src/Permutations/PermutationsOfPalindrome.java
+++ src/Permutations/PermutationsOfPalindrome.java
@@ -1,51 +1,29 @@
 package Permutations;
 
+import java.util.Arrays;
+
 /**
  * Created by Raphael on 11/14/2016.
  */
 public class PermutationsOfPalindrome {
-    public static boolean isPermutationOfPalindrome(String s) {
-        int[] counts = getCharacterCounts(s);
-        if (checkMaximumOneOddChar(counts)) {
-            return true;
-        } else {
-            return false;
+    private static boolean isPalindromePermutation(String str) {
+        str = str.replaceAll("\\s+","");
+
+        boolean[] indexArray = new boolean[256];
+        Arrays.fill(indexArray, true);
+
+        int oddCount = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            int idx = (int) str.charAt(i);
+            indexArray[idx] = !indexArray[idx];
+
+            if (indexArray[idx])
+                oddCount--;
+            else
+                oddCount++;
         }
 
-    }
-
-    private static boolean checkMaximumOneOddChar(int[] charCounts) {
-        int counter = 0;
-        for (int i = 0; i < charCounts.length; i++) {
-            if (charCounts[i] % 2 != 0) {
-                counter++;
-                if (counter > 1) {
-                    return false;
-                }
-            }
-
-        }
-        return true;
-    }
-
-    private static int[] getCharacterCounts(String s) {
-        int[] counts = new int[Character.getNumericValue('z') - Character.getNumericValue('a') + 1];
-        for (char c : s.toCharArray()) {
-            int x = charMap(c);
-            if (x != -1) {
-                counts[x]++;
-            }
-        }
-        return counts;
-    }
-
-    private static int charMap(Character c) {
-        int a = Character.getNumericValue('a');
-        int z = Character.getNumericValue('z');
-        int val = Character.getNumericValue(c);
-        if (a <= val && val <= z) {
-            return val - a;
-        }
-        return -1;
+        return (str.length() % 2 == 0) ? oddCount == 0 : oddCount == 1;
     }
 }
