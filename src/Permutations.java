60ebf957bc0c530752c3e897e217271ab1854758
Raphael Gal <raphaelgal@gmail.com>
2016-11-14 17:21:17 -0500

    added Permutation checker

--- src/Permutations.java
+++ src/Permutations.java
@@ -6,23 +6,6 @@ import java.util.Set;
  * Created by Raphael on 10/30/2016.
  */
 public class Permutations {
-    public static boolean isStringsPermutationsOfEachOther(String a, String b) {
-        int lengthOfA = a.length();
-        int lengthOfB = b.length();
-        Set stringA = new HashSet<>();
-
-        for (int i = 0; i < a.length(); i++) {
-            stringA.add(a.charAt(i));
-        }
-
-        for (int i = 0; i < b.length(); i++) {
-            if (!stringA.contains(b.charAt(i))) {
-                return false;
-            }
-        }
-        LinkedList<Integer>[] arrayOfLinkedLists = new LinkedList[10];
-        return lengthOfA == lengthOfB;
-    }
 
     public static boolean isPermutation(String s1, String s2) {
         if (s1.length() != s2.length()) {
@@ -37,22 +20,5 @@ public class Permutations {
 
         return checker == 0;
     }
-
-//    public static boolean isPermutation(String s1, String s2) {
-//        if (s1.length() != s2.length()) return false;
-//        int checker = 0;
-//        for (int i = 0; i < s1.length(); i++) {
-//            checker ^= s1.charAt(i) ^ s2.charAt(i);
-//        }
-//
-//
-//    /*
-//    sort it, then check the characters if they are two, then two, then two, with allowing one to also be one.
-//    or, check that there is no odds except for one if necessary
-//     */
-//    }
-//    boolean isPermutationOfPalindrome(String phrase) {
-//        int countOdd
-//    }
 
 }
