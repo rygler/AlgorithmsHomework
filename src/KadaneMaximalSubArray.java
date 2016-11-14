f08dadda7ee9caecd49bfd62d7261f3694b038b4
Raphael Gal <raphaelgal@gmail.com>
2016-11-14 17:48:00 -0500

    added Kadane's Algorithm

--- src/KadaneMaximalSubArray.java
+++ src/KadaneMaximalSubArray.java
@@ -3,29 +3,16 @@
  */
 public class KadaneMaximalSubArray {
 
-    public static int localMax(int[] full, int sublength) {
-        int sum = 0;
-        int max = 0;
-        for (int i = sublength; i >= 0; i--) {
-            sum = sum + full[i];
-            if (sum > max) {
-                max = sum;
+    public static int largestSubValues(int[]array){
+        int maxSoFar = 0;
+        int maxIfEndNow = 0;
+        for (int i = 0; i < array.length ; i++) {
+            maxIfEndNow += array[i];
+
+            if(maxIfEndNow > maxSoFar){
+                maxSoFar = maxIfEndNow;
             }
         }
-        sum = 0;
-        return max;
-    }
-
-    public static int splitArr(int[] full) {
-        int sum_final = 0;
-        int max_final = 0;
-        for (int j = 0; j < full.length; j++) {
-            sum_final = localMax(full, j);
-            if (sum_final > max_final) {
-                max_final = sum_final;
-            }
-        }
-
-        return max_final;
+        return maxSoFar;
     }
 }
