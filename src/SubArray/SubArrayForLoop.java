14d391a785fa6340fe275c9199216b553c218a41
Raphael Gal <raphaelgal@gmail.com>
2016-11-14 16:50:21 -0500

    SubArray With For Loop

--- src/SubArray/SubArrayForLoop.java
+++ src/SubArray/SubArrayForLoop.java
@@ -1,15 +1,17 @@
+package SubArray;
+
 import java.util.Arrays;
 
 /**
  * Created by Raphael on 9/13/2016.
  */
-public class SubArrayMatcher {
+public class SubArrayForLoop {
     public static boolean isSubArrayInArray(int[] subArray, int[] array) {
         int subArrayHash = Arrays.hashCode(subArray);
         System.out.println(subArrayHash);
 
         for (int i = 0; i < array.length - subArray.length; i++) {
-            System.out.println(Arrays.hashCode(Arrays.copyOfRange(array, i, i + subArray.length)));
+
             if (subArrayHash == Arrays.hashCode(Arrays.copyOfRange(array, i, i + subArray.length))) {
                 return true;
             }
