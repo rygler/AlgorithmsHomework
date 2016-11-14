22b16b251beb3a9173bfb36f525a125894d765ed
Raphael Gal <raphaelgal@gmail.com>
2016-11-14 18:48:51 -0500

    added Rabin-Karpe

--- src/DictionaryEncoder.java
+++ src/DictionaryEncoder.java
@@ -1,6 +1,54 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by Raphael on 11/14/2016.
  */
 public class DictionaryEncoder {
+    HashMap<Integer, String> dictionary;
+    List<Integer> code;
+    int counter = 0;
 
+    public void encode(String string) {
+        dictionary = new HashMap<>();
+        code = new ArrayList<>();
+
+        String zig = string.substring(0, 1);
+        code.add(counter);
+        dictionary.put(counter++, zig);
+        String wig;
+//        for (int i = 0; i < string.length(); i++) {
+//            zig = string.substring(i, i + 1);
+//            for (int j = i + 1; j < string.length(); j++) {
+//                String wig = string.substring(i, j);
+//                if (!dictionary.containsValue(wig)) {
+//                    dictionary.put(counter++, wig);
+//                }
+//            }
+//        }
+        for (int i = 1; i < string.length(); i++) {
+            wig = string.substring(i, i + 1);
+
+            String zigwig = zig + wig;
+
+            if (dictionary.containsValue(zigwig)){
+                zig = zigwig;
+            } else {
+                code.add(counter);
+                dictionary.put(counter++, zigwig);
+                zig = wig;
+            }
+        }
+
+        for (int i = 0; i < code.size(); i++) {
+            System.out.println(code.get(i) + ": " + dictionary.get(code.get(i)));
+        }
+        dictionary.values().stream().forEach(System.out::println);
+    }
+
+//    public String decode() {
+//
+//    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,18 @@
 import ModularExponentiation.BitShifting;
 import ModularExponentiation.Homegrown;
+import com.sun.org.apache.xpath.internal.SourceTree;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(Homegrown.modularExponentiation(4, 13, 497));
-
-        System.out.println(BitShifting.modularExponentiation(4, 13, 497));
-
-        System.out.println(UniqueString.isUniqueChars("abcdefgarjiok"));
-
-        System.out.println(Permutations.PermutationsMatcher.isPermutation("racecar", "rrcieaa"));
-
+//        System.out.println(Homegrown.modularExponentiation(4, 13, 497));
+//
+//        System.out.println(BitShifting.modularExponentiation(4, 13, 497));
+//
+//        System.out.println(UniqueString.isUniqueChars("abcdefgarjiok"));
+//
+//        System.out.println(Permutations.PermutationsMatcher.isPermutation("racecar", "rrcieaa"));
+        DictionaryEncoder de = new DictionaryEncoder();
+        de.encode("bananaban");
     }
 }
