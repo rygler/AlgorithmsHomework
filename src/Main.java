8d8391a2de7578af4ec87d4ca20a260f177b683a
Raphael Gal <raphaelgal@gmail.com>
2017-01-04 22:43:43 -0500

    updated Genetic Algorithm with Knapsack Problem

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ import Genetics.Genetic;
 public class Main {
 
     public static void main(String[] args) {
-        Genetic genetic = new Genetic(50, 50, 20);
+        new Genetic(50, 50, 20);
     }
 }
